268755b58701db0959425496129824966b7e585c
xoftsisdevloper <xoftsis@gmail.com>
2025-05-24 15:47:07 +0530

    vdsdv

--- LMS-react-backend/backend/server.js
+++ LMS-react-backend/backend/server.js
@@ -58,7 +58,7 @@ app.get('*', (req, res) => {
 })
 
 // Start the server after DB connects
-const PORT = process.env.PORT || 2000
+const PORT = process.env.PORT || 5000
 
 mangoDb().then(() => {
   app.listen(PORT, '0.0.0.0', () => {
